70e159cb8d171b48f0685d4927cd416c93612328
harshx-2005 <theharshkshirsagar@gmail.com>
2026-01-19 01:23:13 +0530

    UI: Update Audio Player to single-row layout

--- frontend/src/components/CustomAudioPlayer.jsx
+++ frontend/src/components/CustomAudioPlayer.jsx
@@ -71,33 +71,31 @@ const CustomAudioPlayer = ({ src }) => {
                 {/* Play/Pause Button */}
                 <button
                     onClick={togglePlay}
-                    className="w-10 h-10 flex items-center justify-center bg-white/20 rounded-full hover:bg-white/30 transition text-white shrink-0 backdrop-blur-md shadow-sm"
+                    className="w-8 h-8 flex items-center justify-center bg-white/20 rounded-full hover:bg-white/30 transition text-white shrink-0 backdrop-blur-md shadow-sm"
                 >
                     {isPlaying ? (
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
                             <path fillRule="evenodd" d="M6.75 5.25a.75.75 0 01.75-.75H9a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H7.5a.75.75 0 01-.75-.75V5.25zm7.5 0A.75.75 0 0115 4.5h1.5a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H15a.75.75 0 01-.75-.75V5.25z" clipRule="evenodd" />
                         </svg>
                     ) : (
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 ml-0.5">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 ml-0.5">
                             <path fillRule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z" clipRule="evenodd" />
                         </svg>
                     )}
                 </button>
 
-                {/* Progress Bar & Time */}
-                <div className="flex-1 flex flex-col justify-center gap-1.5 pt-1">
+                {/* Timeline & Times - Single Row */}
+                <div className="flex-1 flex items-center gap-2">
+                    <span className="text-[10px] text-white/90 font-medium w-6 text-right tabular-nums">{formatTime(currentTime)}</span>
                     <input
                         type="range"
                         min="0"
                         max="100"
                         value={progress}
                         onChange={handleSeek}
-                        className="w-full h-1 bg-white/30 rounded-lg appearance-none cursor-pointer [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-3.5 [&::-webkit-slider-thumb]:h-3.5 [&::-webkit-slider-thumb]:bg-white [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:shadow-sm hover:[&::-webkit-slider-thumb]:scale-110 transition-all"
+                        className="flex-1 h-1 bg-white/30 rounded-lg appearance-none cursor-pointer [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-3 [&::-webkit-slider-thumb]:h-3 [&::-webkit-slider-thumb]:bg-white [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:shadow-sm hover:[&::-webkit-slider-thumb]:scale-110 transition-all"
                     />
-                    <div className="flex justify-between text-[11px] text-white/90 font-medium tracking-wide px-0.5">
-                        <span>{formatTime(currentTime)}</span>
-                        <span>{formatTime(duration)}</span>
-                    </div>
+                    <span className="text-[10px] text-white/90 font-medium w-6 tabular-nums">{formatTime(duration)}</span>
                 </div>
             </div>
         </div>
